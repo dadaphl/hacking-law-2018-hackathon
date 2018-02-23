e5230b59a86a55a68a1cd9c015474d628b99e7fb
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-02-23 12:31:41 +0100

    Add store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import VueResource from 'vue-resource'
 import router from './router'
+import store from './store'
 
 Vue.use(VueResource)
 Vue.config.productionTip = false
@@ -51,5 +52,6 @@ Vue.filter('humanDate', humanDate)
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
