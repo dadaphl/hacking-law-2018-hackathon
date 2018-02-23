5ff288d4f3bd65ffd6841e8c6152f32c6004b017
Philipp Klein <klein.philipp@gmail.com>
2018-02-23 14:37:51 +0100

    docprev compontns

---
+++ src/components/docprev/docprev.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="document">
+    <ae-panel>
+      <div class="authors">
+        <h2>Participants</h2>
+        <div class='author grid' v-for='a in document.authors'>
+          <div><ae-identity-avatar :address='a'/></div>
+          <div>{{a}}</div>
+        </div>
+      </div>
+
+      <div class="versios">
+        <div v-for='v, k in document.versions' class="versio">
+          <ae-divider/>
+            <h2>Version {{k+1}}</h2>
+            signedBy:
+            <div class='author grid' v-for='s in v.signedBy'>
+              <div><ae-identity-avatar :address='s'/></div>
+              <div>{{s}}</div>
+            </div>
+        </div>
+      </div>
+      <div class="center">
+        <ae-button type='exciting' :to='"/view/"'>View Document</ae-button>
+      </div>
+    </ae-panel>
+  </div>
+
+</template>
+
+<script>
+import {
+  AeButton,
+  AeIdentityAvatar,
+  AeDivider,
+  AePanel
+} from '@aeternity/aepp-components'
+export default {
+  props: [
+    'document'
+  ],
+  components: {
+    AeButton,
+    AeDivider,
+    AeIdentityAvatar,
+    AePanel
+  }
+}
+</script>
--- src/screens/index/index.vue
+++ src/screens/index/index.vue
@@ -1,28 +1,50 @@
 <template>
   <div>
-    <ae-panel>
-      <h2>Your Documents</h2>
-      <p class='center'>
-       You dont have any Documents yet, create one
+    <h1>Your Documents</h1>
+      <div v-if='document.length' >
+        <docprev v-for='d in document' :document='d' />
+      </div>
+      <p v-else class='center'>
+         You dont have any Documents yet, create one
       </p>
       <div class="center">
         <ae-button type='exciting' to='/new'>
           Create new Document
         </ae-button>
       </div>
-    </ae-panel>
   </div>
-
 </template>
 <script>
 import {
   AeButton,
+  AeIdentityAvatar,
+  AeDivider,
   AePanel
 } from '@aeternity/aepp-components'
+import Docprev from '../../components/docprev/docprev.vue'
 export default {
   components: {
     AeButton,
-    AePanel
+    AeDivider,
+    AeIdentityAvatar,
+    AePanel,
+    Docprev
+  },
+  computed: {
+    document () {
+      return [
+        {
+          authors: ['0x92e50fFf3e036a4674FE5b2Bc40Fe910777aDe85'],
+          versions: [
+            {
+              content: 'Ipsum cupiditate quos cumque nesciunt possimus? Consequuntur quod itaque delectus error rem Nostrum ex at rerum et veritatis. Dignissimos mollitia maxime nam illum ipsum Laboriosam ducimus enim mollitia quis atque.',
+              signedBy: ['0x92e50fFf3e036a4674FE5b2Bc40Fe910777aDe85']
+            }
+          ]
+        }
+      ]
+      //return this.$store.state.documents
+    }
   }
 }
 </script>
