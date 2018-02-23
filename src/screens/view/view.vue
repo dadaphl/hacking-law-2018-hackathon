617c95276f6e70b7ce0265d028d70defe9d67ea5
Philipp Klein <klein.philipp@gmail.com>
2018-02-23 15:59:37 +0100

    add new version screen

--- src/screens/view/view.vue
+++ src/screens/view/view.vue
@@ -4,6 +4,14 @@
 
     <div class="document" v-if='document'>
       <version v-for='v, i in document.versions' :version='v' :vnum='i'/>
+      <ae-panel>
+        <h2>Create new Version</h2>
+        <froala :tag="'textarea'" :config="config" v-model="model"></froala>
+        <br/>
+        <div class="center">
+          <ae-button type='dramatic' @click="save">save and sign version</ae-button>
+        </div>
+      </ae-panel>
     </div>
   </div>
 </template>
@@ -21,12 +29,32 @@ export default {
     AeIdentityAvatar,
     AePanel
   },
+  data () {
+    return {
+      config: {
+        events: {
+          'froalaEditor.initialized': function () {
+            console.log('initialized')
+          }
+        }
+      },
+      model: '',
+    }
+  },
   computed: {
     id () {
       return this.$route.params.id
     },
     document () {
       return Object.values(this.$store.state.documents).filter(doc => doc.id === this.id)[0]
+    }
+  },
+  methods: {
+    save () {
+      this.$store.dispatch('addVersion', {
+        content: this.model,
+        documentId: this.id
+      })
     }
   }
 }
